1a9671c59f2702f019ae8df253a4702efe966e8b
Zowie <git@zowie.cx>
2025-01-05 15:37:56 +0100

    build: don't clear target/release, unneeded after removal of amqp feature

--- contrib/check.sh
+++ contrib/check.sh
@@ -20,9 +20,6 @@ echo "* running clippy..."
 cargo clippy --quiet
 
 for target in ${TARGETS[@]}; do
-  # clean up the target/release folder otherwise some weird issues happen with GLIBC and serde
-  rm -rf target/release
-
   if [[ "$target" = "$HOST_TARGET" ]]; then
     rustBin=cargo
   else
