c89cbeae1bfbad9fdcfb1d9f925e648d098e05ea
zowie <git@zowie.cx>
2025-04-07 12:49:12 +0200

    fix(build): add nix-community substituter

--- flake.nix
+++ flake.nix
@@ -1,5 +1,12 @@
 {
-  description = "Description for the project";
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
