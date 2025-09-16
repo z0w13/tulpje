8952461cd04036552efee97620aba9e089ca8eeb
zowie <git@zowie.cx>
2025-09-16 22:12:26 +0200

    chore(nix): additional comments and cleanup

--- flake.nix
+++ flake.nix
@@ -43,11 +43,14 @@
           ...
         }:
         let
+          # create the toolchain from `rust-toolchain.toml`
           toolchain = inputs.fenix.packages.${system}.fromToolchainFile {
             file = ./rust-toolchain.toml;
             sha256 = "sha256-Hn2uaQzRLidAWpfmRwSRdImifGUCAb9HeAqTYFXWeQk=";
           };
+          # initialise crane with custom toolchain
           craneLib = ((inputs.crane.mkLib pkgs).overrideToolchain (_: toolchain));
+          # function to build a workspace crate
           buildCrate =
             name:
             pkgs.callPackage ./nix/pkgs/tulpje.nix {
@@ -68,50 +71,52 @@
             ];
           };
 
-          packages.rust-toolchain = toolchain;
+          packages = {
+            rust-toolchain = toolchain;
 
-          packages.tulpje-handler = buildCrate "tulpje-handler";
-          packages.tulpje-gateway = buildCrate "tulpje-gateway";
-          packages.tulpje-manager = buildCrate "tulpje-manager";
-          packages.tulpje-utils = buildCrate "tulpje-utils";
-          packages.twilight-gateway-queue = pkgs.callPackage ./nix/pkgs/twilight-gateway-queue.nix {
-            inherit craneLib;
-          };
-          packages.twilight-http-proxy = pkgs.callPackage ./nix/pkgs/twilight-http-proxy.nix {
-            inherit craneLib;
-          };
+            # project binaries
+            tulpje-handler = buildCrate "tulpje-handler";
+            tulpje-gateway = buildCrate "tulpje-gateway";
+            tulpje-manager = buildCrate "tulpje-manager";
+            tulpje-utils = buildCrate "tulpje-utils";
 
-          packages.default = pkgs.symlinkJoin {
-            name = "tulpje";
-            paths = [
-              self'.packages.tulpje-handler
-              self'.packages.tulpje-gateway
-              self'.packages.tulpje-manager
-              self'.packages.tulpje-utils
-              self'.packages.twilight-gateway-queue
-              self'.packages.twilight-http-proxy
-            ];
-          };
+            # third party binaries
+            twilight-gateway-queue = pkgs.callPackage ./nix/pkgs/twilight-gateway-queue.nix {
+              inherit craneLib;
+            };
+            twilight-http-proxy = pkgs.callPackage ./nix/pkgs/twilight-http-proxy.nix {
+              inherit craneLib;
+            };
 
-          packages.docker-gateway = pkgs.callPackage ./nix/oci-image.nix {
-            main = self'.packages.tulpje-gateway;
-          };
-          packages.docker-handler = pkgs.callPackage ./nix/oci-image.nix {
-            main = self'.packages.tulpje-handler;
-          };
-          packages.docker-gateway-queue = pkgs.callPackage ./nix/oci-image.nix {
-            main = self'.packages.twilight-gateway-queue;
-            utils = self'.packages.tulpje-utils;
-          };
-          packages.docker-http-proxy = pkgs.callPackage ./nix/oci-image.nix {
-            main = self'.packages.twilight-http-proxy;
-            utils = self'.packages.tulpje-utils;
+            # default package that builds all binaries
+            default = pkgs.symlinkJoin {
+              name = "tulpje";
+              paths = [
+                self'.packages.tulpje-handler
+                self'.packages.tulpje-gateway
+                self'.packages.tulpje-manager
+                self'.packages.tulpje-utils
+                self'.packages.twilight-gateway-queue
+                self'.packages.twilight-http-proxy
+              ];
+            };
+
+            # docker images
+            docker-gateway = pkgs.callPackage ./nix/oci-image.nix {
+              main = self'.packages.tulpje-gateway;
+            };
+            docker-handler = pkgs.callPackage ./nix/oci-image.nix {
+              main = self'.packages.tulpje-handler;
+            };
+            docker-gateway-queue = pkgs.callPackage ./nix/oci-image.nix {
+              main = self'.packages.twilight-gateway-queue;
+              utils = self'.packages.tulpje-utils;
+            };
+            docker-http-proxy = pkgs.callPackage ./nix/oci-image.nix {
+              main = self'.packages.twilight-http-proxy;
+              utils = self'.packages.tulpje-utils;
+            };
           };
         };
-      flake = {
-        # The usual flake attributes can be defined here, including system-
-        # agnostic ones like nixosModule and system-enumerating ones, although
-        # those are more easily expressed in perSystem.
-      };
     };
 }
