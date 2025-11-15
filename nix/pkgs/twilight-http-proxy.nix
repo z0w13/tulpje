41db7990639ab5b6b17ce17459efeb3a4b260265
zowie <git@zowie.cx>
2025-11-15 18:11:46 +0100

    chore(http-proxy): update to 0.17.0

--- nix/pkgs/twilight-http-proxy.nix
+++ nix/pkgs/twilight-http-proxy.nix
@@ -7,14 +7,14 @@ let
   src = fetchFromGitHub {
     owner = "twilight-rs";
     repo = "http-proxy";
-    rev = "5d9252e5a2750cd011540db9c81b894effe2f2b0";
-    hash = "sha256-+bDv9tPvBLsFucDDc+kkAvsZkFGxi03JYrZ+9CKjaJw=";
+    rev = "a8d38aef1dee68718280c5af0f062084eb727c20";
+    hash = "sha256-rrAu1Yl/4eyRMalCV2Lck/aSuthGfnUWD2Sms3tWyfU=";
   };
   commonArgs = {
     inherit src;
 
     pname = "twilight-http-proxy";
-    version = "unstable-2025-03-10";
+    version = "0.17.0";
     strictDeps = true;
     cargoExtraArgs = "--features expose-metrics";
   };
