e28bb1d9556a006560d12e374b4d3fddd20622ec
zowie <git@zowie.cx>
2026-02-02 17:55:11 +0100

    refactor(gateway): dedicated function for handling gateway messages

--- gateway/src/shard_manager.rs
+++ gateway/src/shard_manager.rs
@@ -1,11 +1,12 @@
 use futures_util::StreamExt as _;
+use std::error::Error;
 use tokio::{
     sync::mpsc::{self, UnboundedReceiver, UnboundedSender},
     task::JoinHandle,
 };
 use tokio_util::sync::CancellationToken;
 use tulpje_shared::DiscordEvent;
-use twilight_gateway::{CloseFrame, Shard};
+use twilight_gateway::{CloseFrame, Message, Shard};
 
 use crate::{
     metrics,
@@ -24,7 +25,7 @@ pub(crate) struct ShardManagerHandle {
 }
 
 #[derive(PartialEq)]
-enum ShardManagerState {
+enum ShardState {
     Running,
     Stopping,
     Stopped,
@@ -60,6 +61,7 @@ pub(crate) struct ShardManager {
     amqp_tx: UnboundedSender<Vec<u8>>,
     reporter: ShardReporterHandle,
     shutdown: CancellationToken,
+    state: ShardState,
 }
 
 impl ShardManager {
@@ -76,77 +78,30 @@ impl ShardManager {
             amqp_tx,
             reporter,
             shutdown,
+            state: ShardState::Stopped,
         }
     }
 
     async fn run(&mut self) {
         tracing::info!("ShardManager started...");
-        let mut state = ShardManagerState::Stopped;
 
         loop {
             tokio::select! {
                 Some(msg) = self.receiver.recv() => {
                     match msg {
                         ShardManagerMessage::Start => {
-                            state = ShardManagerState::Running;
+                            self.state = ShardState::Running;
                             tracing::info!("Shard started...");
                         },
                     };
                 },
-                msg = self.shard.next(), if state != ShardManagerState::Stopped => {
+                msg = self.shard.next(), if self.state != ShardState::Stopped => {
                     match msg {
                         Some(Ok(message)) => {
-                            let event = match ParsedEvent::from_message(message) {
-                                Ok(evt) => evt,
-                                Err(err) => {
-                                    tracing::warn!("error parsing gateway message: {err}");
-                                    continue;
-                                }
-                            };
-
-                            // if this is a close frame and we're shutting, we break at the
-                            // end of the loop, checking it here to avoid having to clone `event`
-                            let should_stop = event.is_close() && state == ShardManagerState::Stopping;
-
-                            // track event metrics
-                            metrics::track_gateway_event(
-                                self.shard.id().number(),
-                                event.name.as_deref().unwrap_or("default"),
-                            );
-
-                            if let Some(event) = event.event
-                                && let Err(err) = self.reporter
-                                    .try_send(ReporterEvent::from_event(event, self.shard.latency()))
-                            {
-                                tracing::error!("error sending message to ShardManager: {err}");
-                            }
-
-                            if let Some(text) = event.text
-                                && event.forward
-                            {
-                                let event = DiscordEvent::new(self.shard.id().number(), text);
-                                let serialized_event = match serde_json::to_vec(&event) {
-                                    Ok(val) => val,
-                                    Err(err) => {
-                                        tracing::error!("error serializing event: {}", err);
-                                        continue;
-                                    }
-                                };
-
-                                if let Err(err) = self.amqp_tx.send(serialized_event) {
-                                    tracing::error!("error sending event to amqp: {}", err);
-                                    continue;
-                                }
-
-                                tracing::debug!(
-                                    uuid = ?event.meta.uuid,
-                                    shard = event.meta.shard,
-                                    "event sent"
-                                );
-                            }
-
-                            if should_stop {
-                                break
+                            match self.handle_message(message) {
+                                Ok(true) => { break },
+                                Err(err) => tracing::warn!(?err, "error handling message"),
+                                _ => ()
                             }
                         }
                         Some(Err(err)) => {
@@ -158,15 +113,57 @@ impl ShardManager {
                         }
                     }
                 },
-                () = self.shutdown.cancelled(), if state == ShardManagerState::Running => {
+                () = self.shutdown.cancelled(), if self.state == ShardState::Running => {
                     tracing::info!("disconnecting from Discord...");
                     self.shard.close(CloseFrame::RESUME);
-                    state = ShardManagerState::Stopping;
+                    self.state = ShardState::Stopping;
                 },
                 else => break
             }
         }
 
         tracing::info!("ShardManager stopped...");
+    }
+
+    fn handle_message(&mut self, message: Message) -> Result<bool, Box<dyn Error>> {
+        let event = ParsedEvent::from_message(message)
+            .map_err(|err| format!("error parsing gateway message: {err}"))?;
+
+        // if this is a close frame and we're shutting, we break at the
+        // end of the loop, checking it here to avoid having to clone `event`
+        let should_stop = event.is_close() && self.state == ShardState::Stopping;
+
+        // track event metrics
+        metrics::track_gateway_event(
+            self.shard.id().number(),
+            event.name.as_deref().unwrap_or("default"),
+        );
+
+        if let Some(event) = event.event {
+            self.reporter
+                .try_send(ReporterEvent::from_event(event, self.shard.latency()))
+                .map_err(|err| format!("error sending message to ShardManager: {err}"))?;
+        }
+
+        if let Some(text) = event.text
+            && event.forward
+        {
+            let event = DiscordEvent::new(self.shard.id().number(), text);
+
+            let serialized_event = serde_json::to_vec(&event)
+                .map_err(|err| format!("error serializing event: {err}"))?;
+
+            self.amqp_tx
+                .send(serialized_event)
+                .map_err(|err| format!("error sending event to amqp: {err}"))?;
+
+            tracing::debug!(
+                uuid = ?event.meta.uuid,
+                shard = event.meta.shard,
+                "event sent"
+            );
+        }
+
+        Ok(should_stop)
     }
 }
