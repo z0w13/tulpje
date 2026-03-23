81190d2886f0e0249271effdf556d3762b067f09
Zowie <git@zowie.cx>
2026-03-23 22:41:23 +0100

    refactor(handler/pk): use `handle_system_ref` in `/pk setup`

--- handler/src/modules/pk/commands.rs
+++ handler/src/modules/pk/commands.rs
@@ -3,31 +3,27 @@ use reqwest::StatusCode;
 use tulpje_framework::Error;
 
 use super::db;
-use crate::{context::CommandContext, responses};
+use crate::{context::CommandContext, modules::pk::util::handle_system_ref, responses};
 
 // TODO: command to see current settings
-
 pub async fn setup_pk(ctx: CommandContext) -> Result<(), Error> {
     let Some(guild) = ctx.guild().await? else {
         unreachable!("command is guild_only");
     };
 
     ctx.defer_ephemeral().await?;
-
     let user_id = ctx.event.author_id().ok_or("no author?")?;
-    let system_id = ctx.get_arg_string("system_id")?;
 
-    // sanitise and validate system id
-    let system_id = system_id.trim().replace("-", "").to_lowercase();
-    if !system_id.chars().all(|c| char::is_ascii_alphabetic(&c)) {
-        responses::error(&ctx, &format!("### Error\nInvalid system id `{system_id}`")).await?;
+    let Some(system_ref) = handle_system_ref(&ctx, &ctx.get_arg_string("system_id")?).await? else {
         return Ok(());
-    }
+    };
 
-    db::save_guild_settings(&ctx.services.db, guild.id, user_id, &system_id).await?;
-
-    // TODO: fix pkrs to actually handle 404s correctly
-    let system = match ctx.services.pk.get_system(&PkId(system_id.clone())).await {
+    let system = match ctx
+        .services
+        .pk
+        .get_system(&PkId(system_ref.clone().into()))
+        .await
+    {
         Ok(system) => system,
         Err(err)
             if err
@@ -36,7 +32,7 @@ pub async fn setup_pk(ctx: CommandContext) -> Result<(), Error> {
         {
             responses::error(
                 &ctx,
-                &format!("### Error\nCouldn't find system `{system_id}`"),
+                &format!("### Error\nCouldn't find system `{system_ref}`"),
             )
             .await?;
 
@@ -45,14 +41,16 @@ pub async fn setup_pk(ctx: CommandContext) -> Result<(), Error> {
         Err(err) => return Err(err.into()),
     };
 
+    db::save_guild_settings(&ctx.services.db, guild.id, user_id, &system.id.0).await?;
+
     // Inform user of success
     responses::success(
         &ctx,
         &format!(
             "### Success\nPluralKit module setup for {}",
             system.name.map_or_else(
-                || format!("`{}`", system_id),
-                |system_name| format!("{} (`{}`)", system_name, system_id)
+                || format!("`{}`", system.id),
+                |system_name| format!("{} (`{}`)", system_name, system.id)
             )
         ),
     )
