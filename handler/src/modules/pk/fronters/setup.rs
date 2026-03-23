df5c36cec45966c0096d611c80af72d0aed5ecf7
zowie <git@zowie.cx>
2026-03-23 14:27:41 +0100

    feat(handler/pk): check front is public during front category setup

--- handler/src/modules/pk/fronters/setup.rs
+++ handler/src/modules/pk/fronters/setup.rs
@@ -1,3 +1,5 @@
+use pkrs_fork::{client::PkClient, model::PkId};
+use reqwest::StatusCode;
 use tulpje_framework::Error;
 
 use super::{db, shared::create_or_get_fronter_channel};
@@ -14,14 +16,31 @@ pub(crate) async fn handle(ctx: CommandContext) -> Result<(), Error> {
 
     ctx.defer_ephemeral().await?;
 
-    if get_guild_settings_for_id(&ctx.services.db, guild.id)
-        .await?
-        .is_none()
-    {
+    let Some(guild_settings) = get_guild_settings_for_id(&ctx.services.db, guild.id).await? else {
         error_response(&ctx, "PluralKit module not set-up, please run `/pk setup`").await?;
         return Ok(());
     };
 
+    // check for private front list
+    let pk_client = PkClient::default();
+    if let Err(err) = pk_client
+        .get_system_fronters(&PkId(guild_settings.system_id.clone()))
+        .await
+        && let Some(status) = err.status()
+        && status == StatusCode::FORBIDDEN
+    {
+        error_response(
+            &ctx,
+            &format!(
+                "Front for system {} is private, please set it to public to use the fronter list",
+                guild_settings.system_id
+            ),
+        )
+        .await?;
+        return Ok(());
+    };
+
+    // create or get the front category
     let name = ctx.get_arg_string("name")?;
     let fronters_category = create_or_get_fronter_channel(&ctx.client, &guild, name).await?;
 
