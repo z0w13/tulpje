7a7c7747b05c24ff06bf2eb3e7518ee1547facd0
Zowie <git@zowie.cx>
2026-03-24 16:59:24 +0100

    fix(utils/secret-loader): continue without loading secrets if directory is missing

--- utils/src/secret_loader.rs
+++ utils/src/secret_loader.rs
@@ -3,13 +3,17 @@ use std::fs;
 use std::os::unix::process::CommandExt;
 use std::process::Command;
 
-fn main() {
-    let path = std::env::var("SECRET_LOADER_PATH").unwrap_or(String::from("/run/secrets"));
-
-    println!("* injecting env vars from secrets in {} ...", path);
+fn get_secrets(path: &str) -> HashMap<String, String> {
+    let secrets_dir = match fs::read_dir(path) {
+        Ok(dir) => dir,
+        Err(err) => {
+            println!("error accessing secrets dir {path}: {err}");
+            return HashMap::new();
+        }
+    };
 
     let mut env_vars = HashMap::new();
-    for entry in fs::read_dir(path).expect("couldn't read secrets dir") {
+    for entry in secrets_dir {
         let Ok(entry) = entry else {
             println!("error reading path {}", entry.unwrap_err());
             continue;
@@ -27,7 +31,15 @@ fn main() {
         println!("     - {}", var_name);
         env_vars.insert(var_name, var_value.trim().to_owned());
     }
+    env_vars
+}
 
+fn main() {
+    let path = std::env::var("SECRET_LOADER_PATH").unwrap_or(String::from("/run/secrets"));
+
+    println!("* injecting env vars from secrets in {} ...", path);
+
+    let env_vars = get_secrets(&path);
     let args = std::env::args().skip(1).collect::<Vec<String>>();
     let (cmd, args) = args.split_first().expect("no command specified");
 
